2a096df418d863be4f58b6e11eb4545caeaf475b
rossebell <Rosy24dr@outlook.com>
2022-04-19 17:32:33 -0400

    Created the EntriesChartTracker component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import DisplayEntries from "./Components/DisplayEntries/DisplayEntires";
+import DisplayEntries from "./Components/DisplayEntries/DisplayEntries";
 import AddEntryForm from "./Components/AddEntry/AddEntryForm";
+import EntriesChartTracker from "./Components/EntriesChartTracker/EntriesChartTracker";
 
 function App() {
   const [entries, setEntries] = useState([{ weight: 175, date: "11-23-2021" }]);
@@ -11,8 +12,11 @@ function App() {
   }
   return (
     <div>
+      {/* {passing down the information to the child component} */}
       <DisplayEntries parentEntries={entries} /> 
+      {/* {//passing up the information to the child component} */}
       <AddEntryForm addNewEntryProperty={addNewEntry}/>
+      <EntriesChartTracker parentEntries={entries}  />
     </div>
   );
 }
